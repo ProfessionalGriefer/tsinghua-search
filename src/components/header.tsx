b5c05c895dd65a307f645e31f9108c2e5925077e
Vincent Nahn <vincentnahn@gmail.com>
2024-05-14 00:10:28 +0800

    Changed logo from png to webp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="opacity-100 z-100 bg-opacity-100 top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6 container bg-background ">
 
-      <Image width={50} height={50} alt="Tsinghua Logo" src="/tsinghua-logo.png" />
+      <Image width={50} height={50} alt="Tsinghua Logo" src="/tsinghua-logo.webp" />
       <Link href="/" className="font-bold text-xl opacity-100 bg-background">
         Amazing Tsinghua Search
       </Link>
